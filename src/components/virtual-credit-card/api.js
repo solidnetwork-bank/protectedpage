61352828364d3a37bf7b880fa7edf5e84836b5fa
solidnetwork-xyz <2281302+solidnetwork-xyz@users.noreply.github.com>
2024-03-21 13:19:29 +0000

    adds API_GATEWAY_ENDPOINT env var.

--- src/components/virtual-credit-card/api.js
+++ src/components/virtual-credit-card/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiStage = process.env.REACT_APP_API_GATEWAY_STAGE;
+const apiEndpoint = process.env.REACT_APP_API_GATEWAY_ENDPOINT;
 
 export default axios.create({
-  baseURL: `https://api.solidnetwork.xyz/${apiStage}/`
+  baseURL: `${apiEndpoint}/${apiStage}/`
 });
--- src/investments/action/action-const.js
+++ src/investments/action/action-const.js
@@ -1,11 +1,16 @@
+/* API GATEWAY */
+export const API_GATEWAY_ENDPOINT = process.env.REACT_APP_API_GATEWAY_ENDPOINT;
+export const API_GATEWAY_STAGE = process.env.REACT_APP_API_GATEWAY_STAGE;
+export const API_ROOT_PATH = `${API_GATEWAY_ENDPOINT}/${API_GATEWAY_STAGE}`;
+
 /* USER */
 export const AUTH_TOKEN_LOCAL_NAME = '__vrfeqFpHOe__';
 export const AUTH_HEADER = 'Authorization';
-export const API_LOGIN_PATH = 'https://api.solidnetwork.xyz/mock/auth/signin';
+export const API_LOGIN_PATH = `${API_ROOT_PATH}/auth/signin`;
 export const SET_USER_EMAIL = 'SET_USER_EMAIL';
 export const SET_USER_PASSWORD = 'SET_USER_PASSWORD';
 export const SET_USER = 'SET_USER';
-export const USER_IMAGE_PATH = 'https://api.solidnetwork.xyz/mock/profile';
+export const USER_IMAGE_PATH = `${API_ROOT_PATH}/profile`;
 export const SET_USER_IMAGE = 'SET_USER_IMAGE';
 
 /* NAV PATH */
